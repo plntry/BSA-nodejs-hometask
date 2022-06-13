baed5c9386c2ed3ba93ed2a3711257b9783adc1b
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-13 19:38:46 +0300

    Added response middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,15 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
+    // DONE
+    // TODO: Implement middleware that returns result of the query
+    if (res.notFound) {
+        res.status(400).json({ error: true, message: res.message });
+    }
+
+    if (res.badRequest) {
+        res.status(404).json({ error: true, message: res.message });
+    }
+
+    res.status(200).json(res.data); 
     next();
 }
 
