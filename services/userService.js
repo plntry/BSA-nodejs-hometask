196189a62f49feba466e6862f5832c67579d3420
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-13 15:58:27 +0300

    Added userService functions

--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,43 @@
 const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
-
+    // DONE
     // TODO: Implement methods to work with user
+
+    showUserList(users) {
+        const items = UserRepository.getAll(users);
+        if(items.length === 0) {
+            return null;
+        }
+        return items;
+    }
+
+    createUser(input) {
+        const newUser = UserRepository.create(input);
+        if(!newUser) {
+            return null;
+        }
+
+        return newUser;
+    }
+
+    updateUser(id, dataToChange) {
+        const updated = UserRepository.update(id, dataToChange);
+        if(!updated) {
+            return null;
+        }
+        
+        return updated;
+    }
+
+    deleteUser(id) {
+        const deleted = UserRepository.delete(id);
+        if(deleted.length === 0) {
+            return null;
+        }
+
+        return deleted;
+    }
 
     search(search) {
         const item = UserRepository.getOne(search);
