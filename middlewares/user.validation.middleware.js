5a15e864a9c987c328efc0e32b8fbcb8df94e6df
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-13 20:00:58 +0300

    Added create user validation

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,6 +1,46 @@
 const { user } = require('../models/user');
+const { use } = require("chai");
+const { last } = require("lodash");
+
 const createUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during creation
+
+    try {
+        const { firstName, lastName, email, phoneNumber, password } = req.body;
+
+        if (req.body?.id) {
+            throw new Error('Id should not be entered.');
+          }
+
+        if (!(firstName || lastName || email || phoneNumber || password)) {
+            throw new Error('Missed one of the required fields.');
+        }
+
+        Object.keys(req.body).forEach((key) => {
+            if (!user.hasOwnProperty(key)) {
+              throw new Error("There are excessive fields.");
+            }
+        });
+
+        const emailTemp = /^\w+([.-]?\w+)*@gmail.com/;
+        const phoneNumberTemp = /\+380[0-9]{9}$/;
+        
+        if (!email.match(emailTemplate)) {
+            throw new Error(errorMsg);
+        }
+    
+        if (!phoneNumber.match(phoneNumberTemplate)) {
+            throw new Error(errorMsg);
+        }
+    
+        if (password.length < 3) {
+            throw new Error(errorMsg);
+        }
+
+    } catch (error) {
+        res.notFound = true;
+        res.message = error.message;
+    }
 
     next();
 }
