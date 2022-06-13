c22a957d74079312e2402b55ff198465636207dc
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-13 20:14:57 +0300

    Added update user validation

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,6 +2,9 @@ const { user } = require('../models/user');
 const { use } = require("chai");
 const { last } = require("lodash");
 
+const emailTemp = /^\w+([.-]?\w+)*@gmail.com/;
+const phoneNumberTemp = /\+380[0-9]{9}$/;
+
 const createUserValid = (req, res, next) => {
     // DONE
     // TODO: Implement validatior for user entity during creation
@@ -23,19 +26,16 @@ const createUserValid = (req, res, next) => {
             }
         });
 
-        const emailTemp = /^\w+([.-]?\w+)*@gmail.com/;
-        const phoneNumberTemp = /\+380[0-9]{9}$/;
-
         if (password.length < 3) {
-            throw new Error('Invalid input');
+            throw new Error('Invalid input.');
         }
 
         if (!email.match(emailTemp)) {
-            throw new Error('Invalid input');
+            throw new Error('Invalid input.');
         }
     
         if (!phoneNumber.match(phoneNumberTemp)) {
-            throw new Error('Invalid input');
+            throw new Error('Invalid input.');
         }
     } catch (error) {
         res.notFound = true;
@@ -47,6 +47,51 @@ const createUserValid = (req, res, next) => {
 
 const updateUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during update
+
+    try {
+        if (!!req.body?.id) {
+            throw new Error('Id should not be entered.');
+          }
+
+        if (req.body === {}) {
+            throw new Error('Cannot update. There is no data.');
+        }
+
+        Object.keys(req.body).forEach((key) => {
+            if (!Object.keys(user).includes(key))
+                throw new Error('Invalid input.');
+            switch (key) {
+                case "email":
+                if (!req.body.email.match(emailTemplate)) {
+                    throw new Error('Invalid input. Email should be only in gmail format.');
+                }
+                break;
+                case "phoneNumber":
+                if (!req.body.phoneNumber.match(phoneNumberTemplate)) {
+                    throw new Error('Invalid input. Phone number should begin with +380.');
+                }
+                break;
+                case "password":
+                if (req.body.password.length < 3) {
+                    throw new Error('Invalid input. Length of the password should be more than 3.');
+                }
+                break;
+                case "firstName":
+                if (!req.body.firstName) {
+                    throw new Error('Invalid input.');
+                }
+                break;
+                case "lastName":
+                if (!req.body.lastName) {
+                    throw new Error('Invalid input.');
+                }
+                break;
+            }
+        });
+    } catch (error) {
+        res.notFound = true;
+        res.message = error.message;
+    }
 
     next();
 }
