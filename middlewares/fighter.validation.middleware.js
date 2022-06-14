e96429510c7e3d064565335f425b594589832cb5
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-14 12:19:27 +0300

    Added update fighter validation

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -47,7 +47,7 @@ const createFighterValid = (req, res, next) => {
         if (req.body.health < 80 || req.body.health > 120) {
             throw new Error('Invalid input. The amount of health should be from 80 to 120.');
         }
-        
+
         if (!req.body.health) {
             req.body.health = 100;
         }
@@ -62,6 +62,56 @@ const createFighterValid = (req, res, next) => {
 
 const updateFighterValid = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during update
+    
+    try {
+        if (req.body.id) {
+            throw new Error('Id should not be entered.');
+        }
+
+        if (!Object.keys(req.body).length) {
+            throw new Error('Cannot update fighter. There is no data.');
+        }
+
+        Object.keys(req.body).forEach((key) => {
+            if (!Object.keys(user).includes(key))
+                throw new Error('Invalid input.');
+            switch (key) {
+                case "health":
+                    if (typeof req.body.health != "number") {
+                        throw new Error('Invalid input. The amount of health should be a number.');
+                    }
+            
+                    if (req.body.health < 80 || req.body.health > 120) {
+                        throw new Error('Invalid input. The amount of health should be from 80 to 120.');
+                    }
+                    break;
+
+                case "power":
+                    if (typeof power != "number") {
+                        throw new Error('Invalid input. The amount of power should be a number.');
+                    }
+            
+                    if (power < 1 || power > 100) {
+                        throw new Error('Invalid input. The amount of power should be from 1 to 100.');
+                    }
+                    break;
+
+                case "defense":
+                    if (typeof defense != "number") {
+                        throw new Error('Invalid input. The amount of defense should be a number.');
+                    }
+            
+                    if (defense < 1 || defense > 10) {
+                        throw new Error('Invalid input. The amount of defense should be from 1 to 10.');
+                    }
+                    break;
+            }
+        });
+        } catch (error) {
+            res.badRequest = true;
+            res.message = error.message;
+        }
+
     next();
 }
 
