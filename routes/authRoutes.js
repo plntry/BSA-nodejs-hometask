6a2742459f11679ad733fe9e7669dae0fd887ca3
Oksana Nedilko <nedilko7b@gmail.com>
2022-06-14 12:06:26 +0300

    Added authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,13 @@ const router = Router();
 
 router.post('/login', (req, res, next) => {
     try {
+        // DONE
         // TODO: Implement login action (get the user if it exist with entered credentials)
+        const data = AuthService.login(req.body);
         res.data = data;
     } catch (err) {
-        res.err = err;
+        res.notFound = true;
+        res.message = err.message;
     } finally {
         next();
     }
